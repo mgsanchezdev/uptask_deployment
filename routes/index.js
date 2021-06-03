2bdd619d85e7cc36e9821a55776488a029c96161
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-02 21:35:53 -0300

    Create the controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// Import the controller
+const proyectosController = require("../controllers/proyectosController");
+
 module.exports = function () {
   //Route to home
-  router.get("/", (req, res) => {
-    res.json("Index");
-  });
+  router.get("/",proyectosController.proyectosHome);
   return router;
 };
