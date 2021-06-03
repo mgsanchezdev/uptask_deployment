c3aebadc39c34760d051922a6087b6ab9d3f4189
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-03 10:54:59 -0300

    OK form fields

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -12,5 +12,22 @@ exports.formularioProyecto = (req, res) => {
 
 exports.nuevoProyecto = (req, res) => {
   //We are going to send to the console what the user types
-  console.log(req.body)
+
+  //validate that we have something in the field
+  const { nombre } = req.body;
+  let errores = [];
+
+  if(!nombre) {
+      errores.push({'texto': 'Agrega un Nombre al Proyecto'})
+  }
+
+  // si hay errores
+  if(errores.length > 0 ){
+      res.render('nuevoProyecto', {
+          nombrePagina : 'Nuevo Proyecto',
+          errores     
+      })
+    }    else{
+      //No errors, you have to insert in the database
+    }
 };
