30dc74850d2d95ff75a8de808d110b334aa68ec1
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-02 22:20:00 -0300

    create the layout

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
-const path = require ('path')
+const path = require("path");
 //Create an express app
 const app = express();
+
+//Where to load static files
+app.use(express.static("public"));
 
 //Enable pug
 app.set("view engine", "pug");
 
 //Add to view folder
-app.set('views',path.join(__dirname, './views'))
+app.set("views", path.join(__dirname, "./views"));
 
 app.use("/", routes());
 
