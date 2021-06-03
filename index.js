9f6dc7ee62d1605f567e7716e37587ad889c8d05
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-03 15:19:58 -0300

    Create the helpers

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
+const helpers = require("./helpers");
 
 //Create the connection to the database
 const db = require("./config/db");
 
 //import the model
-require('./models/Proyectos')
+require("./models/Proyectos");
 
 db.sync()
   .then(() => {
@@ -25,6 +26,12 @@ app.set("view engine", "pug");
 
 //Add to view folder
 app.set("views", path.join(__dirname, "./views"));
+
+//Pass vardump to the application
+app.use((req, res, next) => {
+  res.locals.vardump = helpers.vardump;
+  next();
+});
 
 //Enable bodyparset for reading form data
 app.use(bodyParser.urlencoded({ extended: true }));
