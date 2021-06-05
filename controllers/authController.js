fdd22c073a722b3bf10ecedd9f572c6525275b6f
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-05 20:48:49 -0300

    protect routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,5 +4,16 @@ exports.autenticarUsuario = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/iniciar-sesion",
   failureFlash: true,
-  badRequestMessage: 'Ambos campos son obligatorios'
+  badRequestMessage: "Ambos campos son obligatorios",
 });
+
+//function to know whether the user is logged in or not
+exports.usuarioAutenticado = (req, res, next) => {
+  //If the user is authenticated, go ahead.
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  //if not authenticated redirect to the form
+  return res.redirect("/iniciar-sesion");
+};
--- routes/index.js
+++ routes/index.js
@@ -15,37 +15,71 @@ const authController = require("../controllers/authController");
 
 module.exports = function () {
   //Route to home
-  router.get("/", proyectosController.proyectosHome);
-  router.get("/nuevo-proyecto", proyectosController.formularioProyecto);
+  router.get(
+    "/",
+    authController.usuarioAutenticado,
+    proyectosController.proyectosHome
+  );
+  router.get(
+    "/nuevo-proyecto",
+    authController.usuarioAutenticado,
+    proyectosController.formularioProyecto
+  );
   router.post(
     "/nuevo-proyecto",
+    authController.usuarioAutenticado,
     body("nombre").not().isEmpty().trim().escape(),
     proyectosController.nuevoProyecto
   );
 
   //list proyect
-  router.get("/proyectos/:url", proyectosController.proyectoPorUrl);
+  router.get(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    proyectosController.proyectoPorUrl
+  );
 
   //Updating the project
-  router.get("/proyecto/editar/:id", proyectosController.fomularioEditar);
+  router.get(
+    "/proyecto/editar/:id",
+    authController.usuarioAutenticado,
+    proyectosController.fomularioEditar
+  );
 
   router.post(
     "/nuevo-proyecto/:id",
+    authController.usuarioAutenticado,
     body("nombre").not().isEmpty().trim().escape(),
     proyectosController.actualizarProyecto
   );
 
   //Delete project
-  router.delete("/proyectos/:url", proyectosController.eliminarProyecto);
+  router.delete(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    proyectosController.eliminarProyecto
+  );
 
   //task
-  router.post("/proyectos/:url", tareasController.agregarTarea);
+  router.post(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    tareasController.agregarTarea
+  );
 
   //update task
-  router.patch("/tareas/:id", tareasController.cambiarEstadoTarea);
+  router.patch(
+    "/tareas/:id",
+    authController.usuarioAutenticado,
+    tareasController.cambiarEstadoTarea
+  );
 
   //delete task
-  router.delete("/tareas/:id", tareasController.eliminarTarea);
+  router.delete(
+    "/tareas/:id",
+    authController.usuarioAutenticado,
+    tareasController.eliminarTarea
+  );
 
   //Create new account
   router.get("/crear-cuenta", usuariosController.formCrearCuenta);
