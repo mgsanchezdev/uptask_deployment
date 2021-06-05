e1d7d370615a3ce6f25060237bac42eb9955e258
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-05 17:03:19 -0300

    unit 22-2

---
+++ config/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+//Refer to the model where we are going to authenticate
+const Usuario = require("../models/Usuarios");
+
+//Local Strgery - Login con credencias propias ( usarui y password)
+
+passport.use(
+  new LocalStrategy(
+    //Por default passport espera un usario y password
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const usuario = await Usuario.find({
+          where: { email: email },
+        });
+        //El usuario existe pero password incorrecto
+        if(usuario.verificarPassword(passport))
+      } catch (error) {
+        //Ese usuario no existe
+        return done(null, false, {
+          message: "Esa cuenta no existe",
+        });
+      }
+    }
+  )
+);
